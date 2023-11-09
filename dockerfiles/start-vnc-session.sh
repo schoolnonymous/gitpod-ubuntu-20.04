e18cd7cff8f11e55f58f4623f6ad5a3e7ad7d27b
schoolnonymous <149703029+schoolnonymous@users.noreply.github.com>
2023-11-09 09:27:58 -0600

    Update start-vnc-session.sh
    fixed it

--- dockerfiles/start-vnc-session.sh
+++ dockerfiles/start-vnc-session.sh
@@ -1,5 +1,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove -y
-sudo apt remove *vnc* -y
+sudo apt purge *vnc* -y
 sudo apt purge *xfce4* -y
+sudo apt purge elementary-i*
+sudo rm xorg.conf
